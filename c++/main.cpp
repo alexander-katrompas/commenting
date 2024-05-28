9d496fbdce431985faf4777405e2a27a152b063c
alexander katrompas <alexander.katrompas@austincc.edu>
2024-05-28 13:11:54 -0500

    stubbed out simple tests for Square class

--- c++/main.cpp
+++ c++/main.cpp
@@ -10,10 +10,13 @@
 int main() {
 
 
-    //Square square1(5);
-    //Square square2(5);
+    Square square1(5);
+    Square square2(10);
 
-    //std::cout << "The area of the square is: " << square.getArea() << std::endl;
+    std::cout << "The area of the square is: " << square1.getArea() << std::endl;
+    std::cout << "The area of the square is: " << square2.getArea() << std::endl;
+    std::cout << "Compare squares: " << compareSquares(square1,square2) << std::endl;
+    std::cout << "Compare squares: " << compareSquares(square2,square1) << std::endl;
 
 
     return 0;
